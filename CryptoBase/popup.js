8fbc5f3fbf5bb1cc2e9321f0600677b7c52d1aae
katiexhancock <katie01225@gmail.com>
2023-01-22 21:59:35 -0800

    Made some changes.

--- CryptoBase/popup.js
+++ CryptoBase/popup.js
@@ -2,4 +2,4 @@ let cursor = document.createElement("div");
 cursor.classList.add("rilakkuma");
 document.body.appendChild(cursor);
 
-let image = chrome.runtime.getURL("cursor16.png")
+let image = chrome.runtime.getURL("cursor16.png");
